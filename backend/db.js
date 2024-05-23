c72b86505cd91b3af2ff934ddcf97008d6e54b3c
Stefano Gargiulo <s.gargiulo@jdk.it>
2024-05-23 20:22:36 +0200

    db migrations

--- backend/db.js
+++ backend/db.js
@@ -5,13 +5,14 @@ let VERSION_DB_MGS = "-0.0.0.0";
 const VERSION_DB_PROMPTS = "-0.0.0.0";
 const VERSION_DB_MEMORY = "";
 
-export const dbVersions = open(
-  {filename: "./stealth_db/versions.db", driver: sqlite3.Database}
-);
+export const dbVersions = open({
+  filename: "./stealth_db/versions.db",
+  driver: sqlite3.Database,
+});
 
 export const setMessageVersion = async (version) => {
   VERSION_DB_MGS = version;
-}
+};
 
 const dbVersionsInit = async () => {
   const db = await dbVersions;
@@ -22,32 +23,39 @@ const dbVersionsInit = async () => {
     )
   `);
   console.log("Database versions initialized");
-  db.run("INSERT OR IGNORE INTO versions (version, key) VALUES (?, ?)", VERSION_DB_MGS, "messages");
-  const versions = await db.all("SELECT * FROM versions WHERE key = 'messages'");
+  db.run(
+    "INSERT OR IGNORE INTO versions (version, key) VALUES (?, ?)",
+    VERSION_DB_MGS,
+    "messages"
+  );
+  const versions = await db.all(
+    "SELECT * FROM versions WHERE key = 'messages'"
+  );
   if (versions.length > 0) {
     VERSION_DB_MGS = versions[0].version;
   }
-}
+};
 
 await dbVersionsInit();
 
 const getMessagesVersion = () => {
   console.log("Messages version is", VERSION_DB_MGS);
   return VERSION_DB_MGS;
-}
+};
 
-export const dbPromise = async() => await open({
-  filename: "./stealth_db/database" + getMessagesVersion() + ".db",
-  driver: sqlite3.Database,
-});
+export const dbPromise = async () =>
+  await open({
+    filename: "./stealth_db/database" + getMessagesVersion() + ".db",
+    driver: sqlite3.Database,
+  });
 
 export const dbPromisePrompts = open({
-  filename: "./stealth_db/prompts"+VERSION_DB_PROMPTS+".db",
+  filename: "./stealth_db/prompts" + VERSION_DB_PROMPTS + ".db",
   driver: sqlite3.Database,
 });
 
 export const dbPromiseMemory = open({
-  filename: "./stealth_db/memory"+VERSION_DB_MEMORY+".db",
+  filename: "./stealth_db/memory" + VERSION_DB_MEMORY + ".db",
   driver: sqlite3.Database,
 });
 
@@ -80,4 +88,16 @@ export const initDb = async () => {
         )
     `);
   console.log("Database initialized");
+  doDbMigrations();
+};
+
+const doDbMigrations = async () => {
+  const db = await dbVersions;
+  //Add fiendlyName column to versions table only if it doesn't exist
+  try {
+    await db.exec(`ALTER TABLE versions ADD COLUMN fiendlyName TEXT`);
+    console.log("Column added");
+  } catch (err) {
+    console.log("Column already exists");
+  }
 };

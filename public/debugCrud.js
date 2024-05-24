374a9c071060c04d37540ce14cd94b59bdc4a694
Stefano Gargiulo <s.gargiulo@jdk.it>
2024-05-24 20:39:56 +0200

    no buttons for debug

--- public/debugCrud.js
+++ public/debugCrud.js
@@ -33,10 +33,6 @@ const fetchDebug = async () => {
             listItem.className = 'list-group-item d-flex justify-content-between align-items-center';
             listItem.innerHTML = `
                 <span class="debug-content">${memory.key}:<br/><pre>${memory.content}</pre></span>
-                <div class="grid grid-cols-2">
-                    <button class="button button-outline mr-1" onclick="editPrompt(${memory.id}, this)"><i class="fas fa-edit"></i></button>
-                    <button class="button button-outline" onclick="deletePrompt(${memory.id})"><i class="fas fa-trash"></i></button>
-                </div>
             `;
             memoryList.appendChild(listItem);
         });
